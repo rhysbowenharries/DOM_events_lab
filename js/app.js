f5d5cfbbcb7abfe46cbf576e87700dbb680e3b99
Ruairidh Holwill <roore@hotmail.co.uk>
2019-05-09 16:12:14 +0100

    mvp done-ish

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
 })
+
+  const handleFormSubmit = function (event) {
+    event.preventDefault();
+
+    const newListItem = document.createElement('li')
+    newListItem.textContent = `Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}` 
+
+    const list = document.querySelector('#reading-list');
+    list.appendChild(newListItem)
+
+
+    document.getElementById('new-item-form').reset();
+  }
